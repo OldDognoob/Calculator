96684d3b20964803ce23ef6e6db9572e548761c9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-13 13:36:15 +0300

    complete-authRouter

---
+++ src/utils/authRouter.js
@@ -0,0 +1,17 @@
+import Rect, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (localStorage.getItem("token")) {
+          return <Component {...props} />;
+        }
+        return <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+export default PrivateRoute;
